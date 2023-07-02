ffc674d22ee17e426e62ed134b6804178141481f
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-02 12:26:39 +0530

    Added Icons and heading with links to linkedin profiles

--- src/App.js
+++ src/App.js
@@ -11,15 +11,38 @@ function App() {
 
   return (
     <>
-      <div className="bgApp grid lg:grid-cols-2 h-screen">
-        <div className="w-full bg1">
-          <LeftComponent
-            setOutput={setOutput}
-            setLoadingState={setLoadingState}
-          />
+      <div className="bgApp grid-rows-auto">
+        <div className="title">
+          <h1>
+            <span className="specialWord">LinkedIn</span> Post-Maker
+          </h1>
         </div>
-        <div className="w-full bg2">
-          <RightComponent output={output} loadingState={loadingState} />
+        <div className="subtitle">
+          <h1>
+            Made with ❣️ -{" "}
+            <span className="specialWord2">
+              <a href="https://www.linkedin.com/in/samyak-jain-3a6639172/">
+                Samyak Jain
+              </a>
+            </span>{" "}
+            and{" "}
+            <span className="specialWord2">
+              <a href="https://www.linkedin.com/in/rutuj-saraf-597994228/">
+                Rutuj Saraf
+              </a>
+            </span>
+          </h1>
+        </div>
+        <div className="grid lg:grid-cols-2 h-screen">
+          <div className="w-full bg1">
+            <LeftComponent
+              setOutput={setOutput}
+              setLoadingState={setLoadingState}
+            />
+          </div>
+          <div className="w-full bg2">
+            <RightComponent output={output} loadingState={loadingState} />
+          </div>
         </div>
       </div>
     </>
